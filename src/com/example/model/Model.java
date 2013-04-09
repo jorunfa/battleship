278f045757a1a3beba497bbe8194b13563fb40e2
Thomas Kjøglum <tkjoeglu@gmail.com>
2013-04-09 09:56:33 +0200

    Changes in Model interface

--- src/com/example/model/Model.java
+++ src/com/example/model/Model.java
@@ -3,5 +3,6 @@ package com.example.model;
 import java.util.Observer;
 
 public interface Model extends Observer {
-
+	public void gameboardPressed(Posistion pos);
+	public void pressedButton(int b);
 }
