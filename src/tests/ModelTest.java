a6082891e406a5dcb8b3991993aede8fd114ef60
Thomas Kjøglum <tkjoeglu@gmail.com>
2013-04-09 13:14:12 +0200

    Added TestModel.java

---
+++ src/tests/ModelTest.java
@@ -0,0 +1,12 @@
+package tests;
+
+import junit.framework.Assert;
+import android.test.AndroidTestCase;
+
+public class ModelTest extends AndroidTestCase {
+
+    public void testSomething() throws Throwable {
+
+    }
+
+}
