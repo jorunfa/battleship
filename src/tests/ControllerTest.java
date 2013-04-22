a2858bc9d1b63a9e9eb2a9ee519e89f427e77fec
Thomas Kjøglum <tkjoeglu@gmail.com>
2013-04-22 10:50:14 +0200

    No changes realy

--- src/tests/ControllerTest.java
+++ src/tests/ControllerTest.java
@@ -5,13 +5,14 @@ import com.example.model.ArrayGameModel;
 import com.example.model.ModelStage;
 import com.example.model.ModelTurn;
 import com.example.model.Position;
+import com.example.starter.SurfaceViewActivity;
 
 import junit.framework.TestCase;
 
 public class ControllerTest extends TestCase {
 
 	public void testReturnCoord() throws Throwable {
-
+		
 		Position aPos = new Position(3, 'b');
 		TouchController aCont = new TouchController();
 
